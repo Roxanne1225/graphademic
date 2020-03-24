ad85cedf568285d89dc3165a36255b4bccbadd19
AlpriElse <alprielse@gmail.com>
2020-03-24 18:22:45 -0500

    Serve client build

---
+++ README.md
@@ -0,0 +1,33 @@
+# Graphademic
+
+## Setup
+
+#### API Setup
+```bash
+cd api
+yarn
+```
+
+#### Client Setup
+```bash
+cd client
+yarn
+```
+  
+## Development  
+
+To start a local instance of the API:
+```bash
+cd api
+yarn dev
+```
+
+To start a local instance of the client:
+```bash
+cd client
+yarn start
+```   
+
+## Deployment 
+
+#### TODO
--- server.js
+++ server.js
@@ -2,8 +2,18 @@ const express = require("express");
 
 const app = express();
 
+
 app.get("/", (req, res) => res.send("Hello World"));
 
-const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+
+}
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
