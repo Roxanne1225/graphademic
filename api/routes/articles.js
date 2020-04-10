4cff06e752a2e4c1494aef073f26e2809daf3c93
AlpriElse <alprielse@gmail.com>
2020-04-09 21:31:06 -0500

    Reformat horribly formatted code

--- api/routes/articles.js
+++ api/routes/articles.js
@@ -14,40 +14,75 @@ const ArticleModel = require('../models/ArticleModel')
 router.get('/', async (req, res) => {
   const queryObject = url.parse(req.url, true).query;
   const articleName = queryObject.articleName;
+
   try {
     const client = await pool.connect();
-    var query = `SELECT aid,fid,citation FROM articles where upper(title)=upper('${articleName}')`;
+    var query = `
+      SELECT aid,fid,citation 
+      FROM articles 
+      WHERE upper(title) = upper('${articleName}')
+    `;
+
     const result = await client.query(query);
-    var articleQueryObject = { authorInfo: [], fieldInfo: [] };
+
+    var articleQueryObject = {
+      authorInfo: [],
+      fieldInfo: []
+    };
+
     if (result && result.rowCount > 0) {
       const aid = result.rows[0].aid;
-      //console.log(aid);
+
       const fid = result.rows[0].fid;
-      query = `SELECT name,title FROM (SELECT rid,title from (SELECT rid,articles.title as title,DENSE_RANK() OVER(PARTITION BY rid ORDER BY articles.citation DESC) as rank FROM
-    (SELECT rid,aid FROM authorizations WHERE rid in (SELECT rid FROM authorizations WHERE aid = ${aid})) as a
-    NATURAL JOIN
-    articles) as b
-    WHERE rank <= 3) as c natural join researchers
-    ORDER BY name`;
+      query = `
+        SELECT name, title 
+        FROM (
+          SELECT rid, title 
+          FROM (
+            SELECT rid, articles.title as title, DENSE_RANK() OVER(PARTITION BY rid ORDER BY articles.citation DESC) as rank 
+            FROM (
+              SELECT rid,aid 
+              FROM authorizations 
+              WHERE rid in (
+                SELECT rid 
+                FROM authorizations 
+                WHERE aid = ${aid}
+              )
+            ) as a NATURAL JOIN articles
+          ) as b
+          WHERE rank <= 3
+        ) as c NATURAL JOIN researchers
+        ORDER BY name
+      `;
+
       const authorInfoObject = await client.query(query);
-      //console.log(authorInfoObject);
+
       if (authorInfoObject && authorInfoObject.rowCount > 0) {
         var curauthorName = authorInfoObject.rows[0].name;
         var curauthor = { name: curauthorName, otherHighlyCitedArticles: [] };
+
         authorInfoObject.rows.forEach(function (item) {
           if (item.name == curauthorName) {
             curauthor.otherHighlyCitedArticles.push(item.title);
           } else {
             articleQueryObject.authorInfo.push(curauthor);
             curauthorName = item.name;
-            curauthor = { name: item.name, otherHighlyCitedArticles: [item.title] };
+            curauthor = {
+              name: item.name,
+              otherHighlyCitedArticles: [item.title]
+            };
           }
         }
         );
         articleQueryObject.authorInfo.push(curauthor);
       }
       if (fid) {
-        query = `SELECT title FROM articles WHERE fid = ${fid} ORDER BY citation DESC LIMIT 5`;
+        query = `
+          SELECT title 
+          FROM articles 
+          WHERE fid = ${fid} 
+          ORDER BY citation DESC LIMIT 5
+        `;
         const fieldarticles = await client.query(query);
         fieldarticles.rows.forEach(function (item) {
           authorQueryObject.fieldInfo.push(item.title);
@@ -55,7 +90,6 @@ router.get('/', async (req, res) => {
       }
     }
     client.release();
-    //  console.log(articleQueryObject);
     res.send(articleQueryObject);
   } catch (err) {
     console.error(err);
@@ -65,31 +99,60 @@ router.get('/', async (req, res) => {
 
 //  POST /articles add
 router.post('/', async (req, res) => {
-
   const frontArticle = req.body;
   const client = await pool.connect();
+
   try {
-    const partialArticle = { title: null, authors: [], publishYear: null, url: null, citations: null, fieldID: null };
+    const partialArticle = {
+      title: null,
+      authors: [],
+      publishYear: null,
+      url: null,
+      citations: null,
+      fieldID: null
+    };
+
     const keys = Object.keys(frontArticle);
     keys.forEach(key => {
       partialArticle[key] = frontArticle[key];
     });
     await client.query('BEGIN');
-    var query = `INSERT INTO articles (title,pub_year,url,citation,fid) values ('${partialArticle.title}',${partialArticle.publishYear},'${partialArticle.url}',${partialArticle.citations},'${partialArticle.fieldID}') RETURNING aid`;
-    const rinserted = await client.query(query);
+    var query = `
+      INSERT INTO articles (title,pub_year,url,citation,fid) values (
+        '${partialArticle.title}',
+        ${partialArticle.publishYear},
+        '${partialArticle.url}',
+        ${partialArticle.citations},
+        '${partialArticle.fieldID}'
+      ) RETURNING aid
+    `;
+
+    const rinsertid = await client.query(query);
     const insertaid = rinsertid.rows[0].aid;
     partialArticle.authors.forEach(async function (item) {
-      query = `SELECT rid FROM researchers WHERE name = '${item}'`;
+      query = `
+        SELECT rid 
+        FROM researchers 
+        WHERE name = '${item}'
+      `;
+
       const result = await client.query(query);
       if (result && result.rowCount > 0) {
         const researcher = result.rows[0].rid;
-        query = `INSERT INTO authorizations (aid,rid) values(${insertaid},${researcher})`;
+        query = `
+          INSERT INTO authorizations (aid,rid) values(${insertaid},${researcher})
+        `;
         await client.query(query);
       } else {
-        query = `INSERT INTO researchers (name) values('${item}') RETURNING rid`;
+        query = `
+          INSERT INTO researchers (name) values('${item}') 
+          RETURNING rid
+        `;
         const finsertid = await client.query(query);
         const finsertrid = finsertid.rows[0].rid;
-        query = `INSERT INTO authorizations (aid,rid) values(${insertaid},${finsertrid})`;
+        query = `
+          INSERT INTO authorizations (aid,rid) values(${insertaid},${finsertrid})
+        `;
         await client.query(query);
       }
     });
@@ -127,14 +190,27 @@ router.put('/:articleId', async (req, res) => {
     const aid = req.params.articleId;
     const partialArticleObject = req.body;
     const keys = Object.keys(partialArticleObject);
-    const dict = { "title": "title", "publishYear": "pub_year", "url": "url", "citations": "citations", "fieldID": "fid" };
+    const dict = {
+      "title": "title",
+      "publishYear": "pub_year",
+      "url": "url",
+      "citations": "citations",
+      "fieldID": "fid"
+    };
+
     keys.forEach(async function (key) {
       if (key == "publishYear" || key == "citations") {
-        const query = `UPDATE articles set ${dict[key]} =  ${partialArticleObject[key]} WHERE aid = ${aid}`;
+        const query = `
+          UPDATE articles set ${dict[key]} =  ${partialArticleObject[key]} 
+          WHERE aid = ${aid}
+        `;
         await client.query(query);
       }
       else {
-        const query = `UPDATE articles set ${dict[key]} =  '${partialArticleObject[key]}' WHERE aid = ${aid}`;
+        const query = `
+          UPDATE articles set ${dict[key]} =  '${partialArticleObject[key]}'
+          WHERE aid = ${aid}
+        `;
         await client.query(query);
       }
     });
@@ -155,7 +231,10 @@ router.delete('/:articleId', async (req, res) => {
   try {
     const client = await pool.connect();
     const aid = req.params.articleId;
-    const query = `DELETE FROM articles WHERE aid = ${aid}`;
+    const query = `
+      DELETE FROM articles 
+      WHERE aid = ${aid}
+    `;
     await client.query(query);
     res.send('OK');
     client.release();
