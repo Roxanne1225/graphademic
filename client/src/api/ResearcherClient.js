068a08afb709d5049d7084e2bc170c2c1bc29265
AlpriElse <alprielse@gmail.com>
2020-05-05 05:34:54 -0500

    Implement researcher delete and edit ui

--- client/src/api/ResearcherClient.js
+++ client/src/api/ResearcherClient.js
@@ -2,6 +2,21 @@ import axios from 'axios'
 
 export function fetchResearcherByResearcherName(researcherName) {
   return axios
-    .get(`/api/researchers?researcherName=${researcherName}`)
+    .get(`/api/researchers/${researcherName}`)
     .then(res => res.data)
+}
+
+export function deleteResearcherByResearcherId(researcherId) {
+  return axios({
+    method: 'DELETE',
+    url: `/api/researchers/${researcherId}`
+  }).then(res => res.data)
+}
+
+export function updateResearcherByResearcherId(researcherId, updateObject) {
+  return axios({
+    method: 'PUT',
+    url: `/api/researchers/${researcherId}`,
+    data: updateObject
+  }).then(res => res.data);
 }
--- client/src/containers/ResearchersContainer.js
+++ client/src/containers/ResearchersContainer.js
@@ -5,7 +5,10 @@ import Container from 'react-bootstrap/Container'
 import Researcher from '../components/researchers/Researcher'
 import ResearcherFilterForm from '../components/researchers/ResearcherFilterForm'
 
-import { fetchResearcherByResearcherName } from '../api/ResearcherClient'
+import {
+  deleteResearcherByResearcherId,
+  fetchResearcherByResearcherName
+} from '../api/ResearcherClient'
 
 const ResearchersContainer = () => {
   const [researcher, setResearcher] = useState(undefined)
@@ -18,13 +21,30 @@ const ResearchersContainer = () => {
     })
   }
 
+  const handleUpdate = (name) => {
+    fetchResearcherByResearcherName(name).then(researcher => {
+      alert(JSON.stringify(researcher))
+      setResearcher(researcher)
+    })
+  }
+
+  const handleDelete = () => {
+    deleteResearcherByResearcherId(researcher.rid).then(article => {
+      alert("Deleted")
+    })
+  }
+
   return (
     <Container className='pt-5'>
       <ResearcherFilterForm onSubmit={handleSubmit} />
       {
         researcher && (
           <div className='pt-3'>
-            <Researcher data={researcher} />
+            <Researcher
+              data={researcher}
+              onDelete={handleDelete}
+              onUpdate={handleUpdate}
+            />
           </div>
         )
       }
