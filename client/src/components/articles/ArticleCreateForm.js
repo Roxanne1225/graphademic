41c261910b2e155ba8fcf18a7a72a8e6a522b702
Peiyi Chen <peiyic2@illinois.edu>
2020-05-06 11:41:22 -0500

    fix enter bar display

--- client/src/components/articles/ArticleCreateForm.js
+++ client/src/components/articles/ArticleCreateForm.js
@@ -1,106 +1,102 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-import Button from 'react-bootstrap/Button'
-import Card from 'react-bootstrap/Card'
-import Form from 'react-bootstrap/Form'
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
 
 const ArticleCreateForm = ({ onCreate }) => {
-  const [article, setArticle] = useState({})
+  const [article, setArticle] = useState({});
 
-  const makeHandleChange = (field, type) => e => {
-    let value = e.target.value
-    if (type === 'integer') {
-      value = parseInt(value)
+  const makeHandleChange = (field, type) => (e) => {
+    let value = e.target.value;
+    if (type === "integer") {
+      value = parseInt(value);
     }
 
     setArticle({
       ...article,
-      [field]: value
-    })
-  }
+      [field]: value,
+    });
+  };
 
-  const handleAuthorsChange = e => {
+  const handleAuthorsChange = (e) => {
     setArticle({
       ...article,
-      authors: e.target.value.split(',')
-    })
-  }
+      authors: e.target.value.split(","),
+    });
+  };
 
-  const handleSubmit = e => {
-    e.preventDefault()
-    onCreate(article)
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onCreate(article);
+  };
 
   return (
     <Card className='mt-5'>
       <Card.Body>
         <Card.Title>Create an Article</Card.Title>
         <Form>
-          <Form.Group controlId="formBasicEmail">
+          <Form.Group controlId='formBasicEmail'>
             <Form.Label>Title</Form.Label>
             <Form.Control
-              onChange={makeHandleChange('title', 'string')}
-              type="text"
-              placeholder="enter a title"
+              onChange={makeHandleChange("title", "string")}
+              type='text'
+              placeholder='enter a title'
             />
           </Form.Group>
 
-          <Form.Group controlId="formBasicPassword">
+          <Form.Group controlId='formBasicPassword'>
             <Form.Label>Authors</Form.Label>
             <Form.Control
               onChange={handleAuthorsChange}
-              type="text"
+              type='text'
               placeholder="enter authors as a list of comma-separated names eg. 'Bob, Joe, Sam'"
             />
           </Form.Group>
 
-          <Form.Group controlId="formBasicPassword">
+          <Form.Group controlId='formBasicPassword'>
             <Form.Label>Year Published</Form.Label>
             <Form.Control
-              onChange={makeHandleChange('publishYear', 'integer')}
-              type="text"
-              placeholder="enter the year published"
+              onChange={makeHandleChange("publishYear", "integer")}
+              type='text'
+              placeholder='enter the year published'
             />
           </Form.Group>
 
-          <Form.Group controlId="formBasicPassword">
+          <Form.Group controlId='formBasicPassword'>
             <Form.Label>URL</Form.Label>
             <Form.Control
-              type="text"
-              onChange={makeHandleChange('url', 'string')}
-              placeholder="enter the URL"
+              type='text'
+              onChange={makeHandleChange("url", "string")}
+              placeholder='enter the URL'
             />
           </Form.Group>
 
-          <Form.Group controlId="formBasicPassword">
+          <Form.Group controlId='formBasicPassword'>
             <Form.Label>Number of Citations</Form.Label>
             <Form.Control
-              onChange={makeHandleChange('citations', 'integer')}
-              type="text"
-              placeholder="enter the number of citations this article has"
+              onChange={makeHandleChange("citations", "integer")}
+              type='text'
+              placeholder='enter the number of citations this article has'
             />
           </Form.Group>
 
-          <Form.Group controlId="formBasicPassword">
+          <Form.Group controlId='formBasicPassword'>
             <Form.Label>Field ID</Form.Label>
             <Form.Control
-              onChange={makeHandleChange('fieldID', 'integer')}
-              type="text"
-              placeholder="enter the number of citations this article has"
+              onChange={makeHandleChange("fieldID", "integer")}
+              type='text'
+              placeholder='enter the Field ID'
             />
           </Form.Group>
 
-          <Button
-            onClick={handleSubmit}
-            variant="primary"
-            type="button"
-          >
+          <Button onClick={handleSubmit} variant='primary' type='button'>
             Create article
           </Button>
         </Form>
       </Card.Body>
     </Card>
-  )
-}
+  );
+};
 
-export default ArticleCreateForm
+export default ArticleCreateForm;
--- client/src/components/researchers/ResearcherCreateForm.js
+++ client/src/components/researchers/ResearcherCreateForm.js
@@ -1,72 +1,68 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-import Button from 'react-bootstrap/Button'
-import Card from 'react-bootstrap/Card'
-import Form from 'react-bootstrap/Form'
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
 
 const ResearcherCreateForm = ({ onCreate }) => {
-  const [researcher, setResearcher] = useState({})
+  const [researcher, setResearcher] = useState({});
 
-  const makeHandleChange = (field, type) => e => {
-    let value = e.target.value
-    if (type === 'integer') {
-      value = parseInt(value)
+  const makeHandleChange = (field, type) => (e) => {
+    let value = e.target.value;
+    if (type === "integer") {
+      value = parseInt(value);
     }
 
     setResearcher({
       ...researcher,
-      [field]: value
-    })
-  }
+      [field]: value,
+    });
+  };
 
-  const handleSubmit = e => {
-    e.preventDefault()
-    onCreate(researcher)
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onCreate(researcher);
+  };
 
   return (
     <Card className='mt-5'>
       <Card.Body>
         <Card.Title>Create a Researcher</Card.Title>
         <Form>
-          <Form.Group controlId="">
+          <Form.Group controlId=''>
             <Form.Label>Name</Form.Label>
             <Form.Control
-              onChange={makeHandleChange('name', 'string')}
-              type="text"
-              placeholder="enter a title"
+              onChange={makeHandleChange("name", "string")}
+              type='text'
+              placeholder='enter a Name'
             />
           </Form.Group>
 
-          <Form.Group controlId="">
+          <Form.Group controlId=''>
             <Form.Label>Institute</Form.Label>
             <Form.Control
-              onChange={makeHandleChange('institute', 'string')}
-              type="text"
-              placeholder="enter a title"
+              onChange={makeHandleChange("institute", "string")}
+              type='text'
+              placeholder='enter a Institute'
             />
           </Form.Group>
 
-          <Form.Group controlId="">
+          <Form.Group controlId=''>
             <Form.Label>PictureURL</Form.Label>
             <Form.Control
-              onChange={makeHandleChange('picture_url', 'string')}
-              type="text"
-              placeholder="enter a title"
+              onChange={makeHandleChange("picture_url", "string")}
+              type='text'
+              placeholder='enter a PictureURL'
             />
           </Form.Group>
 
-          <Button
-            onClick={handleSubmit}
-            variant="primary"
-            type="button"
-          >
+          <Button onClick={handleSubmit} variant='primary' type='button'>
             Create researcher
           </Button>
         </Form>
       </Card.Body>
     </Card>
-  )
-}
+  );
+};
 
-export default ResearcherCreateForm
+export default ResearcherCreateForm;
